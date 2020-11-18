fae8ee0959eb22cd893089066afc6a5379a46c42
SnarkDev <elia.scherrer@gmx.ch>
2020-11-18 03:46:26 -0800

    added delete function

--- src/main/java/ch/zli/m223/punchclock/controller/EntryController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/EntryController.java
@@ -1,6 +1,7 @@
 package ch.zli.m223.punchclock.controller;
 
 import ch.zli.m223.punchclock.domain.Entry;
+import ch.zli.m223.punchclock.repository.EntryRepository;
 import ch.zli.m223.punchclock.service.EntryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -27,5 +28,12 @@ public class EntryController {
     @ResponseStatus(HttpStatus.CREATED)
     public Entry createEntry(@Valid @RequestBody Entry entry) {
         return entryService.createEntry(entry);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteEntry(@PathVariable Long id)
+    {
+        entryService.deleteEntry(id);
     }
 }
