24c584ec8186420b80e2a213a6aed390629f16fe
SnarkDev <elia.scherrer@gmx.ch>
2020-11-26 07:03:15 -0800

    Tried to implement Categories and signupUser function

---
+++ src/main/java/ch/zli/m223/punchclock/controller/CategoryController.java
@@ -0,0 +1,43 @@
+import ch.zli.m223.punchclock.domain.Category;
+import ch.zli.m223.punchclock.service.CategoryService;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Category> getAll() {
+        return categoryService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category create(@Valid @RequestBody Category category) {
+        return categoryService.create(category);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCategory(@PathVariable long id){
+        categoryService.deleteCategory(id);
+    }
+
+    @PutMapping()
+    public Category update(@Valid @RequestBody Category category) {
+        return  categoryService.update(category);
+    }
+}
